643c4ad320305ae6df552c941c07f47cf6f07269
RenanGAS <renansakaoki@gmail.com>
2021-09-07 15:30:55 -0300

    User's welcome implementation

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,23 @@
 let myImage = document.querySelector('img');
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+if (!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName + '!';
+}
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName + '!';
+  }
+}
 
 myImage.onclick = function () {
   let mySrc = myImage.getAttribute('src');
@@ -11,3 +30,8 @@ myImage.onclick = function () {
     console.log("Opa");
   }
 }
+
+myButton.onclick = function () {
+  setUserName();
+}
+
