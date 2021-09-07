5e2da53700b4fdb7ccfa62a1f5a226876a85762d
RenanGAS <renansakaoki@gmail.com>
2021-09-07 14:55:17 -0300

    Fixing navbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,13 @@
 let myImage = document.querySelector('img');
 
-myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox.png') {
-      myImage.setAttribute('src','images/opera.png');
-      myImage.setAttribute('alt','The Opera logo: a red ball with a huge hole.');
-    } else {
-      myImage.setAttribute('src','images/firefox.png');
-    }
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/firefox.png') {
+    myImage.setAttribute('src', 'images/opera.png');
+    myImage.setAttribute('alt', 'The Opera logo: a red ball with a huge hole.');
+  } else if (mySrc === 'images/opera.png') {
+    myImage.setAttribute('src', 'images/firefox.png');
+  } else {
+    console.log("Opa");
+  }
 }
