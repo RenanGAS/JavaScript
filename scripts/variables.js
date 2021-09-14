baa18804ed1ccf9e555cf6fd5ef3174127f52b23
RenanGAS <renansakaoki@gmail.com>
2021-09-13 21:31:15 -0300

    Script that do the greeting of a person and add a cool image to the page when the button is clicked

---
+++ scripts/variables.js
@@ -0,0 +1,11 @@
+const buttonP = document.querySelector('#buttonP');
+const headingT = document.querySelector('#headingT');
+const imageV = document.querySelector('#imageV');
+
+buttonP.onclick = function(){
+    let name = prompt('Digite o seu nome:');
+    alert('Olá ' + name + ', prazer em te conhecer!');
+    headingT.textContent = 'Bem vindo ' + name + '!';
+    imageV.setAttribute('src','images/luffy.jpeg');
+    buttonP.remove();
+}
