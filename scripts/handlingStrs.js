ea12b114ec26fbcd5fc621e5a4cfe49ac3f09b79
RenanGAS <renansakaoki@gmail.com>
2021-09-24 09:47:23 -0300

    Pulling msg from an empty paragraph and fixing the Math.round's range

--- scripts/handlingStrs.js
+++ scripts/handlingStrs.js
@@ -2,6 +2,7 @@ const calcBtn = document.querySelector('#calcBtn');
 const gradeInput = document.querySelector('#nota');
 const testInput = document.querySelector('#prova');
 const msgImg = document.querySelector('#msgImg');
+const msg = document.querySelector('#msg');
 
 calcBtn.addEventListener('click', calcApprobation);
 
@@ -10,12 +11,8 @@ function calcApprobation() {
     let test = Number(testInput.value);
     let average = (70 * test) / 100;
 
-    let msg = document.createElement('p');
     msg.textContent = `${grade >= average ? 'Parabéns, você passou de ano' : 'Não foi dessa vez, mas continue firme'}! 
-    A relação foi de ${grade}/${test} (${Math.round(grade / test) * 100}%)`;
-    msg.style.paddingTop = '3rem';
+    A relação foi de ${grade}/${test} (${Math.round((grade / test) * 100)}%)`;
 
     msgImg.setAttribute('src', `${grade >= average ? 'images/naruto-joinha.jpg' : 'images/naruto-triste.jpg'}`);
-
-    msgImg.insertAdjacentElement('beforebegin', msg);
 }
